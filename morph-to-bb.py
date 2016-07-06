20afcc6fd46fdf41be528ad02f276ad6bac049f9
Jonathan Maw <jonathan.maw@codethink.co.uk>
2016-07-06 16:57:20 +0100

    Don't include default dependencies in packages
    the base bbclass includes some basic packages that everything ought
    to depend on. We're doing our own bootstrap, so we want to override it.

--- morph-to-bb.py
+++ morph-to-bb.py
@@ -305,6 +305,7 @@ BBFILE_PATTERN_baserock := "^${LAYERDIR}/"
     localconf_file = "local.conf.sample"
     localconf_txt = '''\
 MACHINE ??= "qemux86-64"
+INHIBIT_DEFAULT_DEPS = "1"
 '''
     conf_notes_file = "conf-notes.txt"
     conf_notes_txt = '''\
